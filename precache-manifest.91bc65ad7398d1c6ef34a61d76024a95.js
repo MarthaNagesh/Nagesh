94874ed2ceeb1bf6a5d59e1a3a4d757831976c8a
nagesh <marthanagesh@gmail.com>
2020-04-03 00:33:54 -0400

    Updates

---
+++ precache-manifest.91bc65ad7398d1c6ef34a61d76024a95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7d414d605a0ec2c70421878ea79413",
+    "url": "/nagesh/index.html"
+  },
+  {
+    "revision": "c576d2eb4d24abca18e3",
+    "url": "/nagesh/static/css/main.5eb6b148.chunk.css"
+  },
+  {
+    "revision": "70feae53faf4da275709",
+    "url": "/nagesh/static/js/2.1d58171e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nagesh/static/js/2.1d58171e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c576d2eb4d24abca18e3",
+    "url": "/nagesh/static/js/main.7fcdf581.chunk.js"
+  },
+  {
+    "revision": "06ab113e793db5ae4805",
+    "url": "/nagesh/static/js/runtime-main.1b9dbcef.js"
+  },
+  {
+    "revision": "8a93a8a46ef7105325ebaa3fac129b89",
+    "url": "/nagesh/static/media/about.8a93a8a4.jpg"
+  },
+  {
+    "revision": "6f3d9fa692605aa15cfbfdd6c9e858bd",
+    "url": "/nagesh/static/media/background1.6f3d9fa6.jpg"
+  }
+]);
