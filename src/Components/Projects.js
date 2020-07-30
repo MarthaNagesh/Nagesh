73b4102ab98b1fbab7214e089b2c3e5c737998be
Nagesh Martha <nagesh@MacBook-Pro.local>
2020-07-29 23:00:20 -0400

    Added file paths for project1&4

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import img from '../images/about.jpg';
+import img from '../images/Project-1.png';
 import './Projects.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
@@ -16,16 +16,16 @@ const Projects = () => {
                 </div>
                 <div className="projects-item">
                     <div data-aos="fade-up" className="projects-img has-margin-right">
-                        <img src={img} ></img>
+                        <img src={require("../images/Project-1.png")} ></img>
                     </div>
                     <div data-aos="fade-down" className="projects-description">
-                        <h1>Shopping-Cart</h1>
+                        <h1>Portfolio</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                           when an unknown printer took a galley of type and scrambled it to make a type
                            specimen book. 
                         </p>
-                        <a href="#" className="cta">View code</a>
+                        <a target="_new" href="https://github.com/MarthaNagesh/nagesh" className="cta">View code</a>
                         <a target="_new" href="https://marthanagesh.github.io/nagesh/" className="cta">View site</a>
                     </div>
                 </div>
@@ -62,17 +62,17 @@ const Projects = () => {
                 </div>
                 <div className="projects-item">
                 <div data-aos="fade-down" className="projects-description has-margin-right">
-                        <h1>Shopping-Cart</h1>
+                        <h1>expense-Tracker</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                           when an unknown printer took a galley of type and scrambled it to make a type
                            specimen book.
                         </p>
-                        <a href="#" className="cta">View code</a>
-                        <a href="#" className="cta">View site</a>
+                        <a target="_new" href="https://github.com/MarthaNagesh/expenseTracker-React" className="cta">View code</a>
+                        <a target="_new" href="https://marthanagesh.github.io/expenseTracker-React/" className="cta">View site</a>
                     </div>
                     <div data-aos="fade-up" className="projects-img">
-                        <img src={img} ></img>
+                        <img src={require("../images/Project-4.png")} ></img>
                     </div>
                     
                 </div>
